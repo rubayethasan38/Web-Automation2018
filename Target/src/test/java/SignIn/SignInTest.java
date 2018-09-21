907555b3fa4012104e1c0cbf0abe6184c22290a6
lcabrerav <luisjoelcabrera@gmail.com>
2018-09-21 15:32:43 -0400

    testcases

--- Target/src/test/java/SignIn/SignInTest.java
+++ Target/src/test/java/SignIn/SignInTest.java
@@ -9,13 +9,19 @@ import java.util.concurrent.TimeUnit;
 
 public class SignInTest extends CommonAPI {
     @Test
+
     public void SignIn(){
         webDriver.findElement(By.xpath("//*[@id=\"account\"]")).click();
         webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         webDriver.findElement(By.xpath("//*[@id=\"accountNav-signIn\"]/a")).click();
         webDriver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("luisjoelcabrera@gmail.com");
         webDriver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("luillo123", Keys.ENTER);
+    }
+   @Test
+    public void createAnAccount(){
+        webDriver.findElement(By.xpath("//*[@id=\"account\"]")).click();
+        webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        webDriver.findElement(By.xpath("//*[@id=\"accountNav-createAccount\"]/a/div")).click();
 
     }
-
 }
