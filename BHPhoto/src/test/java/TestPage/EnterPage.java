6eab9ca1eccdb3688858cba4deb0f9d76370d5ca
[rubayethasan38] <[rubayet.hasan@outlook.com]>
2018-09-20 20:23:00 -0400

    first'commit

--- BHPhoto/src/test/java/TestPage/EnterPage.java
+++ BHPhoto/src/test/java/TestPage/EnterPage.java
@@ -1,13 +1,19 @@
 package TestPage;
 
 import base.CommonAPI;
-import jdk.internal.jline.internal.TestAccessible;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class EnterPage extends CommonAPI {
     @Test
     public void loginAccount() {
+        webDriver.findElement(By.xpath("//*[@id=\"js-login\"]/span[4]"));
         webDriver.findElement(By.xpath("//*[@id=\"cat989\"]/a/span")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"js-login\"]/span[4]"));
     }
+    @Test
+    public void click() {
+        webDriver.findElement(By.cssSelector("#cat9581 > a > span")).click();
+    }
+
 }
