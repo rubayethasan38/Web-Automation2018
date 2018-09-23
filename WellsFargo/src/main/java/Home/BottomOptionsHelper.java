f9a4812fcb11b8e77e78758b0ed15c9aaaea6589
lcabrerav <luisjoelcabrera@gmail.com>
2018-09-22 23:41:53 -0400

    working

---
+++ WellsFargo/src/main/java/Home/BottomOptionsHelper.java
@@ -0,0 +1,19 @@
+package Home;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class BottomOptionsHelper extends CommonAPI {
+    @FindBy(xpath = "//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[1]/a") public static WebElement AboutWellsFargo;
+    @FindBy(xpath = "//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[3]/a") public static WebElement PrivacyButton;
+    @FindBy(xpath = "//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[4]/a") public static WebElement ReportFraud;
+    @FindBy(xpath = "//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[5]/a") public static WebElement SiteMap;
+    @FindBy(xpath = "//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[6]/a") public static WebElement Accesability;
+    @FindBy(xpath = "//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[7]/a") public static WebElement OnlineAccesment;
+    @FindBy(xpath = "//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[8]/a") public static WebElement AddChoices;
+
+    public void AboutWellsFargoB(){
+        AboutWellsFargo.click();
+    }
+}
--- WellsFargo/src/test/java/BottomOptions/BottomOptionsTest.java
+++ WellsFargo/src/test/java/BottomOptions/BottomOptionsTest.java
@@ -1,54 +1,22 @@
 package BottomOptions;
 
+import Home.BottomOptionsHelper;
 import base.CommonAPI;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class BottomOptionsTest extends CommonAPI {
-   //test30
-   /* @Test
-    public void careersTest(){
-    webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[2]/a")).click();
-    String Expected = "Wells Fargo Careers";
-    String Actual =webDriver.getTitle();
-    System.out.println(Actual);
-    Assert.assertEquals(Actual,Expected);
-    }
- //test31
-    @Test
-  public void aboutWellsFargoB(){
-        webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[1]/a")).click();
-  }
-  //test32
-  @Test
-  public void privacyB(){
-        webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[3]/a")).click();
-}
-   //test33
-    @Test
-    public void reportFraudB(){
-        webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[4]/a")).click();
-}
-   //test34
-    @Test
-    public void siteMapB(){
-        webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[5]/a")).click();
+public class BottomOptionsTest extends BottomOptionsHelper {
 
- }
-    //test35
-    @Test
-    public void accesabitlyB (){
-        webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[6]/a")).click();
- }
- //test36
-   @Test
-   public void onlineAccesmentB(){
-        webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[7]/a")).click();
+    BottomOptionsHelper ObjOfBottomOptionsHelper;
+    @BeforeMethod
+    public void init (){
+        ObjOfBottomOptionsHelper = PageFactory.initElements(webDriver, BottomOptionsHelper.class);
     }
-   //test37
     @Test
-    public void adChoices (){
-        webDriver.findElement(By.xpath("//*[@id=\"pageFooter\"]/div[1]/nav/div/ul/li[8]/a")).click();
-    }*/
+    public void aboutWellsFargoFunction(){
+        AboutWellsFargoB();
+    }
 }
